353cf079bb51aaccd32a75a66b38ad04015e868c
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-02 14:57:55 +0900

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by 강석호 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
